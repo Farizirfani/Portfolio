c3dc14772e8fb440e0dd37d4d2d6868fe1aafa9a
Farizirfani <farizirfani29@gmail.com>
2023-05-10 01:56:53 +0700

    portfolio

---
+++ src/components/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='container'>
+      <div className='h-screen flex abstract-gradient xl:px-32'>
+        <div className='text-white xl:w-1/2 items-center content-center justify-center'>
+          <h1 className='text-5xl'>I'm Fariz Irfani</h1>
+          <h2 className='text-3xl'>Web Developer | Mobile Developer</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem, sint. Lorem ipsum dolor sit amet.</p>
+          <div className='bg-cyan-400 rounded-full px-3 py-4 text-white text-center w-40'>Contact</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
