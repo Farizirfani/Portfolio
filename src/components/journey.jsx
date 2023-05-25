de37d44cab0cc080eed4f0d1ba5b8e7c141ebc0c
Farizirfani <farizirfani29@gmail.com>
2023-05-26 02:50:11 +0700

    Portofolio

--- src/components/journey.jsx
+++ src/components/journey.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 const Journey = () => {
   return (
-    <div>Journey</div>
+    <div className=' py-7'>
+        <h1 className='text-center text-white text-3xl font-semibold'>Let's See My Journey</h1>
+        <div className='py-5'>
+            <VerticalTimeline>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="2008 - 2010"
+                contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}>
+                <div className='bg-slate-600'>
+                    <h1>hello world</h1>
+                </div>
+                <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                <p>
+                User Experience, Visual Design
+                </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="2006 - 2008"
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
+                <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                <p>
+                User Experience, Visual Design
+                </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--education"
+                date="2002 - 2006"
+                iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}>
+                <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+                <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+                <p>
+                Creative Direction, Visual Design
+                </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--education"
+                date="2002 - 2006"
+                iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}>
+                <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+                <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+                <p>
+                Creative Direction, Visual Design
+                </p>
+            </VerticalTimelineElement>
+            </VerticalTimeline>
+        </div>
+    </div>
   )
 }
 
--- src/components/wcid.jsx
+++ src/components/wcid.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Wcid = () => {
   return (
-    <div className='flex md:justify-around justify-center flex-wrap items-center py-7'>
+    <div className='flex md:justify-around justify-center flex-wrap items-center h-screen'>
         <div className='text-white'>
             <h1 className='text-3xl font-semibold'>What can i do?</h1>
             <h2 className='text-xl md:w-[500px] w-auto py-2'>Build and maintain website Frontend Dev  & Mobile Dev</h2>
