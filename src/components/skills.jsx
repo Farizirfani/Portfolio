0e3fc4a5cafe60148446f09fad06bd9f6a713056
Farizirfani <farizirfani29@gmail.com>
2023-05-28 18:26:31 +0700

    Portofolio

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -9,36 +9,59 @@ import nodejs from '../assets/node-js.svg'
 import ReactImg from '../assets/react.svg'
 import VueImg from '../assets/vue.svg'
 import Github from '../assets/github.svg'
+import php from '../assets/php.svg'
+import laravel from '../assets/laravel.svg'
+import flutter from '../assets/flutter.svg'
 
 const Skills = () => {
   return (
     <div className='py-7'>
       <h1 className='text-3xl font-bold text-white text-center'>Skills and Experience</h1>
       <h2 className='text-xl font-semibold text-white text-center py-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, dolore.</h2>
-      <div className='flex flex-wrap text-center justify-center gap-4 content-center items-center'>
-        <div data-aos="fade-up" data-aos-delay="0">
-          <Box image={html} title='HTML'/>
+      <div className='flex flex-wrap text-center justify-center gap-4 content-center items-center py-7'>
+        <div className='' data-aos="fade-up" data-aos-delay="0">
+          <Box image={html}/>
+          <h1 className='text-lg text-white'>HTML</h1>
         </div>
         <div data-aos="fade-up" data-aos-delay="100">
-          <Box  data-aos="fade-up" image={CSS} title='CSS'/>
+          <Box  data-aos="fade-up" image={CSS}/>
+          <h1 className='text-lg text-white'>CSS</h1>
         </div>
         <div data-aos="fade-up" data-aos-delay="200">
-          <Box  data-aos="fade-up" image={js} title='JavaScript'/>
+          <Box  data-aos="fade-up" image={js}/>
+          <h1 className='text-lg text-white'>JavaScript</h1>
         </div>
         <div data-aos="fade-up" data-aos-delay="400">
-          <Box  data-aos="fade-up" image={bootstrap} title='Bootstrap'/>
+          <Box  data-aos="fade-up" image={bootstrap}/>
+          <h1 className='text-lg text-white'>Bootstrap</h1>
+        </div>
+        <div data-aos="fade-up" data-aos-delay="500">
+          <Box image={nodejs}/>
+          <h1 className='text-lg text-white'>node.js</h1>
         </div>
         <div data-aos="fade-up" data-aos-delay="600">
-          <Box image={nodejs} title='node.js'/>
+          <Box image={ReactImg}/>
+          <h1 className='text-lg text-white'>React</h1>
+        </div>
+        <div data-aos="fade-up" data-aos-delay="700">
+          <Box image={VueImg}/>
+          <h1 className='text-lg text-white'>Vue</h1>
         </div>
         <div data-aos="fade-up" data-aos-delay="800">
-          <Box image={ReactImg} title='React'/>
+          <Box className='bg-white rounded-full' image={Github}/>
+          <h1 className='text-lg text-white'>Github</h1>
         </div>
-        <div data-aos="fade-up" data-aos-delay="1000">
-          <Box image={VueImg} title='Vue'/>
+        <div data-aos="fade-up" data-aos-delay="900">
+          <Box image={flutter}/>
+          <h1 className='text-lg text-white'>Flutter</h1>
         </div>
-        <div data-aos="fade-up" data-aos-delay="1200">
-          <Box image={Github} title='Github'/>
+        <div className='p-3' data-aos="fade-up" data-aos-delay="1000">
+          <Box data-aos="fade-up" image={php}/>
+          <h1 className='text-lg text-white'>PHP</h1>
+        </div>
+        <div data-aos="fade-up" data-aos-delay="1100">
+          <Box image={laravel}/>
+          <h1 className='text-lg text-white'>Laravel</h1>
         </div>
       </div>
     </div>
@@ -49,9 +72,9 @@ const Box = (props) => {
   return (
     <div className=' rounded-xl p-2'>
       <div className='w-[80px] m-1'>
-        <img className='' src={props.image} alt="" />
+        <img src={props.image} alt="" />
       </div>
-      <h1 className='text-lg text-white'>{props.title}</h1>
+      {/* <h1 className='text-lg text-white'>{props.title}</h1> */}
     </div>
   )
 }
--- src/components/wcid.jsx
+++ src/components/wcid.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 
+import IconWeb from '../assets/icon-web.png'
+
 const Wcid = () => {
   return (
     <div className='flex md:justify-around justify-center flex-wrap items-center md:h-screen h-auto'>
-        <div className='text-white'>
+        <div className='text-white md:py-2 py-10'>
             <h1 className='text-3xl font-semibold'>What can i do?</h1>
             <h2 className='text-xl md:w-[500px] w-auto py-2'>Build and maintain website Frontend Dev  & Mobile Dev</h2>
             <h2 className='text-xl md:w-[500px] w-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, sapiente.</h2>
             <h2 className='text-xl md:w-[500px] w-auto'>My Motto is Lorem ipsum dolor sit amet.</h2>
         </div>
-        <div  data-aos="fade-up" data-aos-delay="100" className='rounded-md w-[250px] h-[250px] bg-slate-700'>
-        
+        <div  data-aos="fade-up" data-aos-delay="100" className='rounded-md w-[250px] h-[250px] bg-slate-700 my-2 text-center  content-center items-center flex flex-col justify-center'>
+          <div className='rounded-full bg-white w-[75px] p-3 img-wcid'>
+              <img src={IconWeb} alt="" />
+          </div>
+          <h1 className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate?</h1>
         </div>
-        <div className='rounded-md w-[250px] h-[250px] bg-slate-700'>
-        
+        <div  data-aos="fade-up" data-aos-delay="200" className='rounded-md w-[250px] h-[250px] bg-slate-700 my-2 text-center  content-center items-center flex flex-col justify-center'>
+          <div className='rounded-full bg-white w-[75px] p-3 img-wcid'>
+              <img src={IconWeb} alt="" />
+          </div>
+          <h1 className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate?</h1>
         </div>
     </div>
   )
