01e0a0d9567ba8de8dbf040605ca3f4c590134a4
Farizirfani <farizirfani29@gmail.com>
2023-05-29 18:18:07 +0700

    Portofolio

--- src/components/portofolio.jsx
+++ src/components/portofolio.jsx
@@ -6,12 +6,16 @@ const Portofolio = () => {
         <h1 className='text-white font-bold text-2xl'>Project that i has been done</h1>
         <div className='py-5'>
             <div className='flex md:flex-row flex-col-reverse md:justify-between justify-center items-center content-center '>
-                <div>
-                    <h1>testing</h1>
+                <div className='text-white'>
+                    <h1 className='text-2xl font-bold'>Gaming Platform</h1>
+                    <p>This is Gaming platform, developing using React.js, dibuat untuk
+                        mempermudah  <br /> Lorem ipsum dolor sit amet.adass
+                    </p>
+                    <p>Lorem ipsum dolor sit amet.</p>
                 </div>
                 <div>
                     <div className='lg:w-96 w-auto h-auto lg:h-96 bg-slate-600'>
-                        hello world
+                        tes
                     </div>
                 </div>
             </div>
